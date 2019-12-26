69e4ba0361025b9717a6b230c2089e766d76e310
Do-ho <hong7511059@koreatech.ac.kr>
2019-12-27 02:25:48 +0900

    Update app, index css

--- CherryJubilee_FrontEnd/src/App.js
+++ CherryJubilee_FrontEnd/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 
 function App() {
 
@@ -14,92 +15,83 @@ function App() {
   );
 
   const maincolor = { color: 'rgba(164,0,0)'};
+  
+  const thcolor = {
+    color: 'white',
+    backgroundColor: 'rgba(164,0,0)',
+  };
+
+  
 
   return (
     <div className="App">
-
-      <div className="box">
-        <div className="logo">
-          <img src={require("./logo.png")} width="250" height="50"/>
+      <div className="navigation">
+        <div className="container_left">
+          <div className="logo">
+            <img src={require("./logo.png")} width="250" height="50"/>
+          </div>
+        </div>
+        <div className="container_right">
+          <div className="login">
+            <br/>
+            <hi style={maincolor}> 로그인 </hi>
+          </div>
         </div>
       </div>
-      <div className="box">
-        <div className="login">
-          <hi style={maincolor}> 로그인 </hi>
-        </div>
-      </div>
-
-      <div className="after-box">
-        <div className="box2">
-            <h3 style={maincolor}> 근무 리스트 </h3>
+      
+      <div className="container">
+        <div className="container_left">
+          <h3 style={maincolor}> 근무 리스트 </h3>
             <ColoredLine color="rgba(164,0,0)" />
-            <table className="table" align = 'center' border="1">
+            <table className="table" align = 'center'>
               <tr>
-                <td>날짜</td>
-                <td>요일</td>
-                <td>시작시간</td>
-                <td>종료시간</td>
-                <td>총 시간</td>
-                <td>금액</td>
-                <td>수정</td>
+                <td style={thcolor}>날짜</td>
+                <td style={thcolor}>요일</td>
+                <td style={thcolor}>시작시간</td>
+                <td style={thcolor}>종료시간</td>
+                <td style={thcolor}>총 시간</td>
+                <td style={thcolor}>금액</td>
+                <td style={thcolor}>수정</td>
               </tr>
             </table>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
         </div>
-        <div className="box2">
+
+        <div className="container_right">
             <h3 style={maincolor}> 근무 정보 </h3>
             <ColoredLine color="rgba(164,0,0)" />
             <h4 align='center'> 근무지 정보 : 베스킨라빈스</h4>
             <h4 align='center'> 이번 달 금액은 <b>35,000</b>입니다.</h4>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+
             <h3 style={maincolor}> 근무 시간 입력 </h3>
             <ColoredLine color="rgba(164,0,0)" />
-            <table>
+            <table align='center'>
               <tr>
-                <td><input type='text'></input></td>
-                <td><input type='text'></input></td>
+                <td><input type='text' className='textbox'></input></td>
+                <td><input type='text' className='textbox'></input></td>
                 <td>~</td>
-                <td><input type='text'></input></td>
-              </tr>
-              <tr>
-                <button>입력</button>
-                <button>초기화</button>
+                <td><input type='text' className='textbox'></input></td>
               </tr>
             </table>
+            <div align='right'>
+              <button className='inputButton'>입력</button>
+              <button className='resetButton'>리셋</button>
+            </div>
             <br/>
             <h3 style={maincolor}> 지난 근무 </h3>
             <ColoredLine color="rgba(164,0,0)" />
             <br/>
-            <table className="table" align = 'center' border="1">
+            <table className="table" align = 'center'>
               <tr>
-                <td>주차</td>
-                <td>총 시간</td>
-                <td>금액</td>
+                <td style={thcolor}>주차</td>
+                <td style={thcolor}>총 시간</td>
+                <td style={thcolor}>금액</td>
               </tr>
             </table>
           </div>
+
       </div>
+      
+        
     </div>
   );
 }
